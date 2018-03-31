d1c63090742b4f97e7eec25df2760c23cdbc10f8
divastavisca <dagarwal@tavisca.com>
2018-03-31 11:18:50 +0530

    added search contracts

---
+++ Custom.Weather.Contracts/GeoCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Custom.Weather.Contracts
+{
+    public class GeoCode
+    {
+        public double Latitude { get; private set; }
+
+        public double Longitude { get; private set; }
+
+        public GeoCode(double latitude, double longitude)
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+        }
+    }
+}
---
+++ Custom.Weather.Contracts/SearchRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom.Weather.Contracts
+{
+    public class SearchRequest
+    {
+        public string Name { get; set; }
+
+        public GeoCode Coordinates { get; set; }
+    }
+}
