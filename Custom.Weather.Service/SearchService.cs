f225b9be6351e804ea0bffd9fdff0f8df7e78300
snarayan <snarayan@TAVRENTLAP18>
2018-03-31 13:14:17 +0530

    added service layer

---
+++ Custom.Weather.Service/SearchService.cs
@@ -0,0 +1,15 @@
+﻿using Custom.Weather.Contracts.Interfaces;
+using System;
+using Custom.Weather.Contracts;
+using System.Threading.Tasks;
+
+namespace Custom.Weather.Service
+{
+    public class SearchService : IWeatherSearchService
+    {
+        public Task<SearchResponse> SearchAsync(SearchRequest searchRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
