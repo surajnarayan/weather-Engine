2596f2c8c25d86a6206fce712c6a18af4a5facef
snarayan <snarayan@TAVRENTLAP18>
2018-03-31 12:26:27 +0530

    Added searchResponse

---
+++ Custom.Weather.Contracts/SearchResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Custom.Weather.Contracts
+{
+    public class SearchResponse
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+
+        public string Tempurature { get; set; }
+
+        public string Pressure { get; set; }
+
+        public string Humidity { get; set; }
+
+        public string TempMin { get; set; }
+
+        public string TempMax { get; set; }
+
+        public string WindSpeed { get; set; }
+
+    }
+}
