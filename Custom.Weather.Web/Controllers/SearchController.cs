3c5cca81b8fa0070fbd18d4a30b977b6bd86faf3
snarayan <snarayan@TAVRENTLAP18>
2018-03-31 13:13:15 +0530

    modified search controller

--- Custom.Weather.Web/Controllers/SearchController.cs
+++ Custom.Weather.Web/Controllers/SearchController.cs
@@ -1,46 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Custom.Weather.Contracts.Interfaces;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Custom.Weather.Contracts;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Custom.Weather.Web.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/search")]
     public class SearchController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        private IWeatherSearchService 
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async  Task<IActionResult> Search([FromBody]SearchRequest searchRequest)
         {
+
+           
         }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
